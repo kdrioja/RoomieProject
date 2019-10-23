5c6eca85babcd08fffc2e265b65a126dabbca1bb
Kenia Rioja-Naranjo <kdrioja@gmail.com>
2019-10-22 19:19:16 -0700

    Working on login and signup functionalities.

--- app/src/main/java/com/example/roomie/MainActivity.java
+++ app/src/main/java/com/example/roomie/MainActivity.java
@@ -1,14 +1,81 @@
 package com.example.roomie;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+/**
+ * Login activity
+ * Can either lead to home page if logged in successfully or
+ * Signup activity if it is a new user
+ */
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth mFirebaseAuth;
+    FirebaseUser mFireBaseUser;
+    EditText email, password;
+    TextView errorMessage;
+    Button signupButton, loginButton;
+
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // get the instance from Firebase
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.emailEditText);
+        password = findViewById(R.id.passwordEditText);
+        errorMessage = findViewById(R.id.errorMessageTextView);
+        signupButton = findViewById(R.id.signupButton);
+        loginButton = findViewById(R.id.logonButton);
+
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                mFireBaseUser = mFirebaseAuth.getCurrentUser();
+
+                if (mFireBaseUser != null) {
+                    Toast.makeText(MainActivity.this, "You are logged in.", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(MainActivity.this, HomeActivity.class));
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Please log in", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+
+
+        // if login button is clicked then check email and password and proceed to home screen
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+
+        // if signup button is clicked then we need to transition to the sign up page
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentSignup = new Intent(MainActivity.this, SignupActivity.class);
+                startActivity(intentSignup);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/roomie/SignupActivity.java
+++ app/src/main/java/com/example/roomie/SignupActivity.java
@@ -1,14 +1,92 @@
 package com.example.roomie;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+/**
+ * Signup activity
+ * Leads to the home page if new user is successfully created
+ */
 
 public class SignupActivity extends AppCompatActivity {
+
+    FirebaseAuth mFirebaseAuth;
+    EditText firstName, lastName, email, password;
+    Button signupButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        signupButton = findViewById(R.id.signupButton);
+        firstName = findViewById(R.id.firstNameEditText);
+        lastName = findViewById(R.id.lastNameEditText);
+        email = findViewById(R.id.emailEditText);
+        password = findViewById(R.id.passwordEditText);
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String firstString = firstName.getText().toString();
+                String lastString = lastName.getText().toString();
+                String emailString = email.getText().toString();
+                String passwordString = password.getText().toString();
+
+                // First we check that all fields have been completed
+                if (firstString.isEmpty()) {
+                    firstName.setError("Required");
+                    firstName.requestFocus();
+                }
+                else if (lastString.isEmpty()) {
+                    lastName.setError("Required");
+                    lastName.requestFocus();
+                }
+                else if (emailString.isEmpty()) {
+                    /**
+                     * TODO: check that it is a valid email
+                     */
+                    email.setError("Required");
+                    email.requestFocus();
+                }
+                else if (passwordString.isEmpty()) {
+                    password.setError("Required");
+                    password.requestFocus();
+                }
+                else if (!(firstString.isEmpty() && lastString.isEmpty() && emailString.isEmpty() && passwordString.isEmpty())) {
+                    // no field was left empty
+                    // Creates the new user account
+                    mFirebaseAuth.createUserWithEmailAndPassword(emailString, passwordString).addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(SignupActivity.this, "Sign up was successful!", Toast.LENGTH_SHORT).show();
+
+                                startActivity(new Intent(SignupActivity.this, HomeActivity.class));
+                            }
+                            else {
+                                Toast.makeText(SignupActivity.this, "Sign up was unsuccessful, please try again.", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+                else {
+                    // to catch any errors
+                    Toast.makeText(SignupActivity.this, "Error occurred, please try again.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
