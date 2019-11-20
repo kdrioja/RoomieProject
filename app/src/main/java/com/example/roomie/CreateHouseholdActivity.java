6e7d6efb3b6d2f878263fdaac694167e3fcad31e
Kenia Rioja-Naranjo <kdrioja@gmail.com>
2019-11-20 11:39:31 -0800

    Can now check if email entered when creating new household is in DB

--- app/src/main/java/com/example/roomie/CreateHouseholdActivity.java
+++ app/src/main/java/com/example/roomie/CreateHouseholdActivity.java
@@ -11,6 +11,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -29,10 +31,11 @@ public class CreateHouseholdActivity extends AppCompatActivity {
     TextView emailsTextView;
 
     List<String> roommateIDs;
-    //int emailsCount;
 
     private FirebaseDatabase mFirebaseDatabase;
     private DatabaseReference mDatabaseReference;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFireBaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,10 +51,14 @@ public class CreateHouseholdActivity extends AppCompatActivity {
         emailsTextView = findViewById(R.id.emailsTextView);
 
         roommateIDs = new ArrayList<>();
-        //emailsCount = 0;
 
         mFirebaseDatabase = FirebaseDatabase.getInstance();
         mDatabaseReference = mFirebaseDatabase.getReference();
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFireBaseUser = mFirebaseAuth.getCurrentUser();
+
+        //Clear ipsum text from TextView
+        emailsTextView.setText("");
 
         //On Click Listeners
         addButton.setOnClickListener(new View.OnClickListener() {
@@ -75,59 +82,98 @@ public class CreateHouseholdActivity extends AppCompatActivity {
             }
         });
 
+        //On creation add the current user's uid since they are a roommate
+        roommateIDs.add(mFireBaseUser.getUid());
+        emailsTextView.append(mFireBaseUser.getEmail());
     }
 
+    /**
+     * Cancels the creation of a new household object
+     * @param view
+     */
     public void onCancelClicked(View view) {
         startActivity(new Intent(CreateHouseholdActivity.this, NoHouseholdActivity.class));
     }
 
-    public void onCreateClicked(View view) {
-
-    }
-
     /**
-     * Handles the adding of
+     * Saves list of roommates user ids
+     * Updates TextView to display added emails
      * @param view
      */
     public void onAddClicked(View view) {
         final DatabaseReference emailsTableReference = mDatabaseReference.child("emails");
-        final String newEmail = emailEditText.getText().toString();
+        final String inputEmail = emailEditText.getText().toString();
+        //final String commaEmail = emailEditText.getText().toString().replace('.', ',');
 
-        if (newEmail.isEmpty()) {
+        if (inputEmail.isEmpty()) {
             emailEditText.setError("Required");
             emailEditText.requestFocus();
         }
         else {
             //Check if email is in database
             //Replace all '.' with ','
-            String emailComma = newEmail;
-            emailComma.replace('.', ',');
+            final String commaEmail = emailEditText.getText().toString().replace('.', ',');
 
-            emailsTableReference.child(emailComma).addListenerForSingleValueEvent(new ValueEventListener() {
+            //emailsTableReference.child(commaEmail);
+
+
+            emailsTableReference.child(commaEmail).addListenerForSingleValueEvent(new ValueEventListener() {
                 @Override
                 public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                    //email is in DB, get userID associated with it
-                    String userID = dataSnapshot.getValue(String.class);
+                    /*
+                    Returns the value of the child if exists, otherwise -> null
 
-                    //add user ID to list of user IDs in this household
-                    roommateIDs.add(userID);
+                    dataSnapshot.toSting()
+                    DataSnapshot {key = pni@gmail,com, value = u2WTwPgSZzZB0mAcniA9xLmLBnx2}
+                     */
 
-                    //Display the email in the text view
-                    if (roommateIDs.size() == 1) {
-                        emailsTextView.append(newEmail);
+                    //emailsTextView.append(dataSnapshot.toString() + " /// ");
+
+
+                    if (dataSnapshot.getValue() == null) {
+                        //Child doesn't exist
+                        emailEditText.getText().clear();
+                        Toast.makeText(CreateHouseholdActivity.this,
+                                "Email not registered", Toast.LENGTH_SHORT).show();
                     }
                     else {
-                        emailsTextView.append(", " + newEmail);
+                        // Check if user ID is already in the rommateIDs list
+                        if (roommateIDs.contains(dataSnapshot.getValue().toString())) {
+                            emailEditText.getText().clear();
+                            Toast.makeText(CreateHouseholdActivity.this,
+                                    "Email already added", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            //add user ID to list of user IDs in this household
+                            roommateIDs.add(dataSnapshot.getValue().toString());
+
+                            //emailsTextView.append(dataSnapshot.getValue().toString() + " ::: ");
+                            //emailsTextView.append(dataSnapshot.toString() + " // ");
+
+                            //Display the email in the text view
+                            emailsTextView.append(", " + inputEmail);
+
+                            //Clear the email input field
+                            emailEditText.getText().clear();
+                        }
                     }
                 }
 
                 @Override
                 public void onCancelled(@NonNull DatabaseError databaseError) {
-                    //email is not in DB, inform user through an error
-                    emailEditText.getText().clear();
-                    Toast.makeText(CreateHouseholdActivity.this, "User not found", Toast.LENGTH_SHORT).show();
+                    //ERROR
                 }
             });
         }
+    }
+
+    /**
+     * Creates a household object
+     * Adds the household object to the households table
+     * Adds the household-id to each of the user's tables
+     * @param view
+     */
+    public void onCreateClicked(View view) {
+
     }
 }
